0b3b4eb4070d9d167a84f3c5b43dc11f1c571695
Pettersom Polonio <105749407+pppettersom@users.noreply.github.com>
2025-09-09 09:45:45 -0300

    Banco de dados e templates para edição de usuários

--- HelloWorld/app/Hello/routes.py
+++ HelloWorld/app/Hello/routes.py
@@ -1,10 +1,36 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, redirect,url_for
+from ..models import User, db
+
 
 hello_bp  = Blueprint('hello',__name__)
 
 @hello_bp.route('/')
 def index():
-    return "Hello Word"
-@hello_bp.route('/sobre')
-def about():
-    return "Olá, Pettersom!"
+    usuarios = User.query.all()
+    return render_template('index.html', usuarios=usuarios)
+
+@hello_bp.route('/novoUsuario',methods=['POST'])
+def novoUsuario():
+    nome_usuario = request.form['nome_usuario']
+
+    novo_usuario = User(username=nome_usuario)
+    db.session.add(novo_usuario)
+    db.session.commit()
+    return redirect('/')
+
+@hello_bp.route('/removerUsuario/<int:usuario_id>', methods=['POST'])
+def removerUsuario(usuario_id):
+    usuario = User.query.get(usuario_id)
+    if usuario:
+        db.session.delete(usuario)
+        db.session.commit()
+    return redirect(url_for('hello.index'))
+
+@hello_bp.route('/editarUsuario/<int:usuario_id>', methods=['POST'])
+def editarUsuario(usuario_id):
+    usuario  = User.query.get(usuario_id)
+    if usuario:
+        usuario.username = request.form['nome_usuario']
+        db.session.commit()
+    return redirect(url_for('hello.index'))
+
--- HelloWorld/app/__init__.py
+++ HelloWorld/app/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from .Hello.routes import hello_bp
+from .models import db, User
 
 def create_app():
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hello.db'
+    db.init_app(app)
 
+    with app.app_context():
+        db.create_all()
+        
     app.register_blueprint(hello_bp)
 
     return app
